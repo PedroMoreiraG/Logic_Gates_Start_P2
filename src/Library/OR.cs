5740037424b48c4280d108ed786d008e59d2b1db
PedroMoreiraG <pedro.moreira@correo.ucu.edu.uy>
2024-09-16 21:01:37 -0300

    OR

---
+++ src/Library/OR.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace Library;
+
+public class OR
+{
+    private string name;
+    private ArrayList entrada;
+    private ArrayList nameEntry;
+    
+    public string Name
+    {
+        get { return this.name; }
+        set { this.name = value; }
+    }
+
+    public OR(String name)
+    {
+        this.name = name;
+    }
+
+    public void AgregarEntrada(string nameEntry, int entrada1)
+    {
+        entrada.Add(entrada1);
+        if (entrada.Count>2)
+        {
+            entrada.Clear();
+            entrada.Add(entrada1);
+        }
+    }
+    
+    public int Calcular()
+    {
+        
+        if (Convert.ToInt32(entrada[0]) == 1 || Convert.ToInt32(entrada[1]) == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
