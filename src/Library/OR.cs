ab6bf234cf1f612bb93e51f30545ecc4397da0f4
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2024-09-18 18:56:28 -0300

    .

--- src/Library/OR.cs
+++ src/Library/OR.cs
@@ -4,39 +4,45 @@ namespace Library;
 
 public class OR
 {
-    private string name;
-    private ArrayList entrada;
-    private ArrayList nameEntry;
+    private string nombre;
+    private ArrayList listaLetras;
+    private ArrayList listaValores;
     
-    public string Name
+    public string Nombre
     {
-        get { return this.name; }
-        set { this.name = value; }
+        get { return this.nombre; }
+        set { this.nombre = value; }
     }
 
+    
     public OR(String name)
     {
-        this.name = name;
+        this.nombre = name;
     }
 
-    public void AgregarEntrada(string nameEntry, int entrada1)
+    
+    public void AgregarEntrada(string letra, int valor)
     {
-        entrada.Add(entrada1);
-        if (entrada.Count>2)
+        if (listaValores.Count >= 2)
         {
-            entrada.Clear();
-            entrada.Add(entrada1);
+            listaValores.Clear();
+            listaLetras.Clear();
         }
+        listaValores.Add(valor);
+        listaLetras.Add(letra);
     }
 
+    
     public string getNombre()
     {
-        return this.name;
+        return this.nombre;
     }
+    
+    
     public int Calcular()
     {
         
-        if (Convert.ToInt32(entrada[0]) == 1 || Convert.ToInt32(entrada[1]) == 1)
+        if (Convert.ToInt32(listaValores[0]) == 1 || Convert.ToInt32(listaValores[1]) == 1)
         {
             return 1;
         }
