0c4dedffec2b9486e17c36b33271269a664c0ef0
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2024-09-18 18:42:51 -0300

    Saco atributos y constructores innecesarios en garagegate

--- src/Library/GarageGate.cs
+++ src/Library/GarageGate.cs
@@ -8,36 +8,21 @@ public class GarageGate
     private AND CompuertaAND = new AND("AND-1");
     private OR CompuertaOR = new OR("OR-1");
     private NOT CompuertaNOT = new NOT("NOT-1");
-    private string name;
-    private ArrayList listaLetras;
-    private ArrayList listaValores;
-
     
-    public string Name
-    {
-        get { return this.name; }
-        set { this.name = value; }
-    }
-
-    public GarageGate()
-    {
-    }
     
     //Utilizo las clases para actuar segun la compuerta logica de c, b, a
     public int Calcular(string c, int valorC, string b, int valorB, string a, int valorA)
     {
         CompuertaAND.AgregarEntrada(a, valorA);
         CompuertaAND.AgregarEntrada(b, valorB);
-        int resultadoAND = CompuertaAND.Calcular();
         
-        CompuertaNOT.AgregarEntrada(CompuertaAND.getNombre(), resultadoAND);
-        int resultadoNOT = CompuertaNOT.Calcular();
         
-        CompuertaOR.AgregarEntrada(CompuertaAND.getNombre(), resultadoAND);
-        CompuertaOR.AgregarEntrada(CompuertaNOT.getNombre(), resultadoNOT);
-        int resultadoOR = CompuertaOR.Calcular();
+        CompuertaNOT.AgregarEntrada(CompuertaAND.getNombre(), CompuertaAND.Calcular());
+        
+        CompuertaOR.AgregarEntrada(CompuertaAND.getNombre(), CompuertaAND.Calcular());
+        CompuertaOR.AgregarEntrada(CompuertaNOT.getNombre(), CompuertaNOT.Calcular());
 
-        CompuertaAND.AgregarEntrada(CompuertaOR.getNombre(), resultadoOR);
+        CompuertaAND.AgregarEntrada(CompuertaOR.getNombre(), CompuertaOR.Calcular());
         CompuertaAND.AgregarEntrada(c, valorC);
         return CompuertaAND.Calcular();
     }
