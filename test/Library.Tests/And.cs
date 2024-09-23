3fbf20f4f2ab713f019d2fbb2b4fa549f94412cf
PedroMoreiraG <pedro.moreira@correo.ucu.edu.uy>
2024-09-23 20:28:16 -0300

    Agrego UML

--- test/Library.Tests/And.cs
+++ test/Library.Tests/And.cs
@@ -4,12 +4,12 @@ namespace Library.Tests;
 public class ANDTest
 {
     [Test]
-    public void Test1()
+    public void AndTest()
     {
         AND primerAnd = new AND("and1");
         primerAnd.AgregarEntrada("a",1);
         primerAnd.AgregarEntrada("b",1);
         
-        Assert.AreEqual(0, primerAnd.Calcular());
+        Assert.AreEqual(1, primerAnd.Calcular());
     }
 }
